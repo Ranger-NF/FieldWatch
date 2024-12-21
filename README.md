5ec149f47d222ee9649337cce1222308a920b024
Ranger <yemzero0@gmail.com>
2024-12-21 16:18:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Name
+## Team: Neuronauts
+
---
+++ esp32/httpcode.ino
@@ -0,0 +1,59 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// Replace with your Wi-Fi credentials
+const char* ssid = "Neuronauts";
+const char* password = "neuroftw";
+
+// Server URL
+const char* serverUrl = "http://ptsv3.com/t/neuronauts/";
+
+// Create an instance of HTTPClient
+HTTPClient http;
+
+void setup() {
+  // Start serial communication
+  Serial.begin(115200);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi!");
+}
+
+void loop() {
+  // Ensure Wi-Fi is connected
+  if (WiFi.status() == WL_CONNECTED) {
+    // Start HTTP connection
+    http.begin(serverUrl);
+    
+    // Set the Content-Type header for JSON
+    http.addHeader("Content-Type", "application/json");
+
+    // Create a simple JSON payload
+    String payload = "{\"message\": \"Hello from Xiao ESP32-S3!\"}";
+    
+    // Send POST request
+    int httpResponseCode = http.POST(payload);
+
+    // Check the response from the server
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Response from server:");
+      Serial.println(response);  // This will print the server's response
+    } else {
+      Serial.println("Error in sending POST request");
+    }
+    
+    // Close the HTTP connection
+    http.end();
+  } else {
+    Serial.println("Wi-Fi not connected");
+  }
+
+  delay(10000);  // Wait for 10 seconds before sending the next request
+}
