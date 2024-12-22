70d09a6895fdd5f7d63121d97f84d492b4e08b5d
rishinandan ks <rishinandan2007@gmail.com>
2024-12-22 13:29:22 +0530

    ✨ Added lastest model data

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,16 +1,150 @@
-#include "pitches.h"
-#include "esp_camera.h"
-#include <Arduino.h>
-#include <driver/i2s.h>
+#include <Sound_Effect_inferencing.h>
 
-// I2S configuration
-#define I2S_DATA_IN_PIN 34  // Adjust to your microphone input pin
-#define I2S_CLOCK_PIN 0
-#define I2S_LRCK_PIN 26
-#define I2S_SCK_PIN 27
 
-#include "edge_impulse_sdk.h"  // Include Edge Impulse SDK
+/* Edge Impulse Arduino examples
+ * Copyright (c) 2022 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * This code was adapted by Marcelo Rovai to run on a XIAO ESP32S3
+ * 29May23
+ * Modified by Salman Faris
+ * 14Aug23
+ * 
+ */
 
+// If your target is limited in memory remove this macro to save 10K RAM
+#define EIDSP_QUANTIZE_FILTERBANK 0
+
+/*
+ ** NOTE: If you run into TFLite arena allocation issue.
+ **
+ ** This may be due to may dynamic memory fragmentation.
+ ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
+ ** if it doesn't exist) and copy this file to
+ ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
+ **
+ ** See
+ ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
+ ** to find where Arduino installs cores on your machine.
+ **
+ ** If the problem persists then there's not enough memory for this model and application.
+ */
+
+/* Includes ---------------------------------------------------------------- */
+#include <I2S.h>
+#define SAMPLE_RATE 16000U
+#define SAMPLE_BITS 16
+
+/*************************************************
+   Public Constants
+ *************************************************/
+
+#define NOTE_B0 31
+#define NOTE_C1 33
+#define NOTE_CS1 35
+#define NOTE_D1 37
+#define NOTE_DS1 39
+#define NOTE_E1 41
+#define NOTE_F1 44
+#define NOTE_FS1 46
+#define NOTE_G1 49
+#define NOTE_GS1 52
+#define NOTE_A1 55
+#define NOTE_AS1 58
+#define NOTE_B1 62
+#define NOTE_C2 65
+#define NOTE_CS2 69
+#define NOTE_D2 73
+#define NOTE_DS2 78
+#define NOTE_E2 82
+#define NOTE_F2 87
+#define NOTE_FS2 93
+#define NOTE_G2 98
+#define NOTE_GS2 104
+#define NOTE_A2 110
+#define NOTE_AS2 117
+#define NOTE_B2 123
+#define NOTE_C3 131
+#define NOTE_CS3 139
+#define NOTE_D3 147
+#define NOTE_DS3 156
+#define NOTE_E3 165
+#define NOTE_F3 175
+#define NOTE_FS3 185
+#define NOTE_G3 196
+#define NOTE_GS3 208
+#define NOTE_A3 220
+#define NOTE_AS3 233
+#define NOTE_B3 247
+#define NOTE_C4 262
+#define NOTE_CS4 277
+#define NOTE_D4 294
+#define NOTE_DS4 311
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_FS4 370
+#define NOTE_G4 392
+#define NOTE_GS4 415
+#define NOTE_A4 440
+#define NOTE_AS4 466
+#define NOTE_B4 494
+#define NOTE_C5 523
+#define NOTE_CS5 554
+#define NOTE_D5 587
+#define NOTE_DS5 622
+#define NOTE_E5 659
+#define NOTE_F5 698
+#define NOTE_FS5 740
+#define NOTE_G5 784
+#define NOTE_GS5 831
+#define NOTE_A5 880
+#define NOTE_AS5 932
+#define NOTE_B5 988
+#define NOTE_C6 1047
+#define NOTE_CS6 1109
+#define NOTE_D6 1175
+#define NOTE_DS6 1245
+#define NOTE_E6 1319
+#define NOTE_F6 1397
+#define NOTE_FS6 1480
+#define NOTE_G6 1568
+#define NOTE_GS6 1661
+#define NOTE_A6 1760
+#define NOTE_AS6 1865
+#define NOTE_B6 1976
+#define NOTE_C7 2093
+#define NOTE_CS7 2217
+#define NOTE_D7 2349
+#define NOTE_DS7 2489
+#define NOTE_E7 2637
+#define NOTE_F7 2794
+#define NOTE_FS7 2960
+#define NOTE_G7 3136
+#define NOTE_GS7 3322
+#define NOTE_A7 3520
+#define NOTE_AS7 3729
+#define NOTE_B7 3951
+#define NOTE_C8 4186
+#define NOTE_CS8 4435
+#define NOTE_D8 4699
+#define NOTE_DS8 4978
 
 // notes in the melody:
 int melody[] = {
@@ -22,116 +156,252 @@ int noteDurations[] = {
   4, 8, 8, 4, 4, 4, 4, 4
 };
 
-// Camera configuration settings (adjust according to your needs)
-camera_config_t config = {
-  .pin_pwdn = GPIO_NUM_32,
-  .pin_reset = GPIO_NUM_NC,
-  .pin_xclk = GPIO_NUM_0,
-  .pin_sda = GPIO_NUM_26,
-  .pin_scl = GPIO_NUM_27,
-  .pin_d0 = GPIO_NUM_5,
-  .pin_d1 = GPIO_NUM_18,
-  .pin_d2 = GPIO_NUM_19,
-  .pin_d3 = GPIO_NUM_21,
-  .pin_d4 = GPIO_NUM_36,
-  .pin_d5 = GPIO_NUM_39,
-  .pin_d6 = GPIO_NUM_34,
-  .pin_d7 = GPIO_NUM_35,
-  .pin_vsync = GPIO_NUM_25,
-  .pin_href = GPIO_NUM_23,
-  .pin_pclk = GPIO_NUM_22,
-  .xclk_freq_hz = 10000000,  // 10 MHz
-  .ledc_timer = LEDC_TIMER_0,
-  .ledc_channel = LEDC_CHANNEL_0,
-  .brightness = 0,
-  .contrast = 0,
-  .saturation = 0,
-  .whitebal = 0,
-  .gainceiling = (gainceiling_t)0,
-  .pixel_format = PIXFORMAT_JPEG,
-  .frame_size = FRAMESIZE_SVGA,  // Choose a frame size
-  .jpeg_quality = 12,
-  .fb_count = 2
-};
+/** Audio buffers, pointers and selectors */
+typedef struct {
+  int16_t *buffer;
+  uint8_t buf_ready;
+  uint32_t buf_count;
+  uint32_t n_samples;
+} inference_t;
 
+static inference_t inference;
+static const uint32_t sample_buffer_size = 2048;
+static signed short sampleBuffer[sample_buffer_size];
+static bool debug_nn = false;  // Set this to true to see e.g. features generated from the raw signal
+static bool record_status = true;
 
-// Your inference function
-void run_inference(uint8_t *image_buffer, size_t image_size) {
-  // Prepare the input for Edge Impulse model
-  // This might include resizing or other preprocessing if necessary
-
-  // Run the inference
-  ei_impulse_result_t result = ei_run_impulse(image_buffer, image_size);
-
-  // Handle the result (e.g., print the classification result)
-  Serial.print("Inference Result: ");
-  Serial.println(result.classification[0].value); // This is just an example
-}
-
-void play_note() {
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-    // to calculate the note duration, take one second divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(1, melody[thisNote], noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    noTone(1);
-  }
-}
-
+/**
+ * @brief      Arduino setup function
+ */
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-    // Initialize camera
-  esp_camera_init(&config);
 
-    i2s_config_t i2s_config = {
-    .mode = I2S_MODE_MASTER | I2S_MODE_RX,  // Receive mode
-    .sample_rate = 16000,  // 16 kHz sample rate
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,  // 16-bit samples
-    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,  // Mono audio
-    .communication_format = I2S_COMM_FORMAT_I2S_MSB,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,  // Interrupt flag
-    .dma_buf_count = 8,  // Buffer count
-    .dma_buf_len = 1024,  // Buffer length
-    .use_apll = false,
-  };
+  pinMode(LED_BUILTIN, OUTPUT);
+  // comment out the below line to cancel the wait for USB connection (needed for native USB)
+  while (!Serial)
+    ;
+  Serial.println("Edge Impulse Inferencing Demo");
+
+  I2S.setAllPins(-1, 42, 41, -1, -1);
+  if (!I2S.begin(PDM_MONO_MODE, SAMPLE_RATE, SAMPLE_BITS)) {
+    Serial.println("Failed to initialize I2S!");
+    while (1)
+      ;
+  }
+
   
-  i2s_pin_config_t pin_config = {
-    .ws_io_num = I2S_LRCK_PIN,
-    .bck_io_num = I2S_SCK_PIN,
-    .data_in_num = I2S_DATA_IN_PIN,
-    .data_out_num = I2S_PIN_NO_CHANGE,  // Output not used
-  };
 
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &pin_config);
-}
-void loop() {
-  int16_t buffer[1024];  // Buffer to hold the audio data
-  size_t bytes_read;
-  
-  // Read audio data from the I2S bus
-  i2s_read(I2S_NUM_0, (void*)buffer, sizeof(buffer), &bytes_read, portMAX_DELAY);
+  // summary of inferencing settings (from model_metadata.h)
+  ei_printf("Inferencing settings:\n");
+  ei_printf("\tInterval: ");
+  ei_printf_float((float)EI_CLASSIFIER_INTERVAL_MS);
+  ei_printf(" ms.\n");
+  ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+  ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+  ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
 
-  // Capture a frame from the camera
-  camera_fb_t *fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Camera capture failed");
+  ei_printf("\nStarting continious inference in 2 seconds...\n");
+  ei_sleep(2000);
+
+  if (microphone_inference_start(EI_CLASSIFIER_RAW_SAMPLE_COUNT) == false) {
+    ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
     return;
   }
 
-  // Send the frame to Edge Impulse model (inference step)
-  // Example: Use a C++ Edge Impulse library to process the frame data
-  run_inference(fb->buf, fb->len);
-
-  // Return the frame buffer to the camera driver
-  esp_camera_fb_return(fb);
-
-  delay(100); // Adjust delay for real-time processing
+  ei_printf("Recording...\n");
 }
 
+void playSound() {
+  // iterate over the notes of the melody:
+  // for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+  //   // to calculate the note duration, take one second divided by the note type.
+  //   //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+  //   int noteDuration = 1000 / noteDurations[thisNote];
+  //   tone(1, melody[thisNote], noteDuration);
+
+  //   // to distinguish the notes, set a minimum time between them.
+  //   // the note's duration + 30% seems to work well:
+  //   int pauseBetweenNotes = noteDuration * 1.30;
+  //   delay(pauseBetweenNotes);
+  //   // stop the tone playing:
+  //   noTone(1);
+  // }
+
+    for (int i = 0; i < 10; i++) {
+    int frequency = random(100, 1000);  // Random frequency between 100Hz and 1000Hz
+    tone(1, frequency);  // Play the tone
+    delay(500);  // Delay for 100ms before changing frequency
+    noTone(1);  // Stop the tone
+    delay(100);  // Short pause between tones
+  }
+}
+
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
+void loop() {
+  bool m = microphone_inference_record();
+  if (!m) {
+    ei_printf("ERR: Failed to record audio...\n");
+    return;
+  }
+
+  signal_t signal;
+  signal.total_length = EI_CLASSIFIER_RAW_SAMPLE_COUNT;
+  signal.get_data = &microphone_audio_signal_get_data;
+  ei_impulse_result_t result = { 0 };
+
+  EI_IMPULSE_ERROR r = run_classifier(&signal, &result, debug_nn);
+  if (r != EI_IMPULSE_OK) {
+    ei_printf("ERR: Failed to run classifier (%d)\n", r);
+    return;
+  }
+
+  int pred_index = 0;    // Initialize pred_index
+  float pred_value = 0;  // Initialize pred_value
+
+  // print the predictions
+  ei_printf("Predictions ");
+  ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+  ei_printf(": \n");
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    ei_printf("    %s: ", result.classification[ix].label);
+    ei_printf_float(result.classification[ix].value);
+    ei_printf("\n");
+
+    if (result.classification[ix].value > pred_value) {
+      pred_index = ix;
+      pred_value = result.classification[ix].value;
+    }
+  }
+
+    // Display inference result
+    if ((pred_index == 0 && (pred_value > 0.8))) {
+      playSound();
+    } else if ((pred_index == 2) && (pred_value > 0.8)) {
+      digitalWrite(LED_BUILTIN, LOW);  //Turn off
+    } else {
+      //nothing to do. 
+    }
+
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("    anomaly score: ");
+    ei_printf_float(result.anomaly);
+    ei_printf("\n");
+#endif
+  }
+
+  static void audio_inference_callback(uint32_t n_bytes) {
+    for (int i = 0; i < n_bytes >> 1; i++) {
+      inference.buffer[inference.buf_count++] = sampleBuffer[i];
+
+      if (inference.buf_count >= inference.n_samples) {
+        inference.buf_count = 0;
+        inference.buf_ready = 1;
+      }
+    }
+  }
+
+  static void capture_samples(void *arg) {
+
+    const int32_t i2s_bytes_to_read = (uint32_t)arg;
+    size_t bytes_read = i2s_bytes_to_read;
+
+    while (record_status) {
+
+      /* read data at once from i2s */
+      esp_i2s::i2s_read(esp_i2s::I2S_NUM_0, (void *)sampleBuffer, i2s_bytes_to_read, &bytes_read, 100);
+
+      if (bytes_read <= 0) {
+        ei_printf("Error in I2S read : %d", bytes_read);
+      } else {
+        if (bytes_read < i2s_bytes_to_read) {
+          ei_printf("Partial I2S read");
+        }
+
+        // scale the data (otherwise the sound is too quiet)
+        for (int x = 0; x < i2s_bytes_to_read / 2; x++) {
+          sampleBuffer[x] = (int16_t)(sampleBuffer[x]) * 8;
+        }
+
+        if (record_status) {
+          audio_inference_callback(i2s_bytes_to_read);
+        } else {
+          break;
+        }
+      }
+    }
+    vTaskDelete(NULL);
+  }
+
+  /**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+  static bool microphone_inference_start(uint32_t n_samples) {
+    inference.buffer = (int16_t *)malloc(n_samples * sizeof(int16_t));
+
+    if (inference.buffer == NULL) {
+      return false;
+    }
+
+    inference.buf_count = 0;
+    inference.n_samples = n_samples;
+    inference.buf_ready = 0;
+
+    //    if (i2s_init(EI_CLASSIFIER_FREQUENCY)) {
+    //        ei_printf("Failed to start I2S!");
+    //    }
+
+    ei_sleep(100);
+
+    record_status = true;
+
+    xTaskCreate(capture_samples, "CaptureSamples", 1024 * 32, (void *)sample_buffer_size, 10, NULL);
+
+    return true;
+  }
+
+  /**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+  static bool microphone_inference_record(void) {
+    bool ret = true;
+
+    while (inference.buf_ready == 0) {
+      delay(10);
+    }
+
+    inference.buf_ready = 0;
+    return ret;
+  }
+
+  /**
+ * Get raw audio signal data
+ */
+  static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr) {
+    numpy::int16_to_float(&inference.buffer[offset], out_ptr, length);
+
+    return 0;
+  }
+
+  /**
+ * @brief      Stop PDM and release buffers
+ */
+  static void microphone_inference_end(void) {
+    free(sampleBuffer);
+    ei_free(inference.buffer);
+  }
+
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
